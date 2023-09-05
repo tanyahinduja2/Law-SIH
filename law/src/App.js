e22ebe7d5d83f01571291b063ec022437a108a9b
Varun <varun.jajoo18@gmail.com>
2023-09-05 12:34:02 +0530

    commit

--- law/src/App.js
+++ law/src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
-
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import { Description } from "./components/Description";
 function App() {
-  return <div className="App">Hello World</div>;
+  return <div className="App">
+    <Navbar/>
+    <Hero/>
+    <Description/>
+  </div>;
 }
 
 export default App;
