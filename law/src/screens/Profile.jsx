2bba10994af7ac54b78a3ff38032125769e2a119
sanjanalalwani <610sanjana@gmail.com>
2023-09-06 19:34:51 +0530

    init

--- law/src/screens/Profile.jsx
+++ law/src/screens/Profile.jsx
@@ -1,22 +1,33 @@
-import React,{useEffect} from 'react'
-import { getAuth, onAuthStateChanged } from "firebase/auth";
+import React , { Component } from 'react';
+ import "../css/profile.css";
+
+// import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 const Profile = () => {
-    const auth = getAuth();
+    // const auth = getAuth();
 
-useEffect(() => {
-  const unsubscribe = onAuthStateChanged(auth, (user) => {
-    if (user) {
-      console.log("User is signed in:", user);
-    } else {
-      console.log("User is signed out");
-    }
-  });
-  return () => unsubscribe();
-}, []);
+// useEffect(() => {
+//   const unsubscribe = onAuthStateChanged(auth, (user) => {
+//     if (user) {
+//       console.log("User is signed in:", user);
+//     } else {
+//       console.log("User is signed out");
+//     }
+//   });
+//   return () => unsubscribe();
+// }, []);
 
   return (
- <div></div>
+    <div class="wrapper">
+    <aside class="aside">Profile Page
+    <br />
+    <img src="https://img.freepik.com/premium-vector/account-icon-user-icon-vector-graphics_292645-552.jpg?" alt="" />
+    </aside>
+    <div class="left">Your Notes
+    </div>
+    <div class="content">Your Documents</div>
+    <div class="right">Scheduled Meetings</div>
+ </div>
   )
 }
 
