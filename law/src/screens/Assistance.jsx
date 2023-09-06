0d759d0a230a0a96da0d5aab776178291cc6d212
Varun <varun.jajoo18@gmail.com>
2023-09-06 12:13:00 +0530

    commit

--- law/src/screens/Assistance.jsx
+++ law/src/screens/Assistance.jsx
@@ -1,13 +1,99 @@
-import React from 'react'
-import '../css/chat.css'
+import React from "react";
+import "../css/chat.css";
 export default function Assistance() {
   return (
-    <div className='rectangle'>
-    <div className="pfp"></div>
-    <div className="name">
-        <div className="expertise"></div>
-    </div>
-    <button className='btn'>Chat</button>
-    </div>
-  )
+    <>
+      <div className="rectangle">
+        <div className="pfp"></div>
+        <div className="name">
+          Varun Jajoo
+          <div className="expertise">Divorce Lawyer</div>
+        </div>
+        <button
+          className="btn chat
+    "
+        >
+          Chat
+        </button>
+      </div>
+      <div className="rectangle">
+        <div className="pfp"></div>
+        <div className="name">
+          Varun Jajoo
+          <div className="expertise">Divorce Lawyer</div>
+        </div>
+        <button
+          className="btn chat
+    "
+        >
+          Chat
+        </button>
+      </div>
+      <div className="rectangle">
+        <div className="pfp"></div>
+        <div className="name">
+          Varun Jajoo
+          <div className="expertise">Divorce Lawyer</div>
+        </div>
+        <button
+          className="btn chat
+    "
+        >
+          Chat
+        </button>
+      </div>
+      <div className="rectangle">
+        <div className="pfp"></div>
+        <div className="name">
+          Varun Jajoo
+          <div className="expertise">Divorce Lawyer</div>
+        </div>
+        <button
+          className="btn chat
+    "
+        >
+          Chat
+        </button>
+      </div>
+      <div className="rectangle">
+        <div className="pfp"></div>
+        <div className="name">
+          Varun Jajoo
+          <div className="expertise">Divorce Lawyer</div>
+        </div>
+        <button
+          className="btn chat
+    "
+        >
+          Chat
+        </button>
+      </div>
+      <div className="rectangle">
+        <div className="pfp"></div>
+        <div className="name">
+          Varun Jajoo
+          <div className="expertise">Divorce Lawyer</div>
+        </div>
+        <button
+          className="btn chat
+    "
+        >
+          Chat
+        </button>
+      </div>
+      <div className="rectangle">
+        <div className="pfp"></div>
+        <div className="name">
+          Varun Jajoo
+          <div className="expertise">Divorce Lawyer</div>
+        </div>
+        <button
+          className="btn chat
+    "
+        >
+          Chat
+        </button>
+      </div>
+    </>
+  );
 }
