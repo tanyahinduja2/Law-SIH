e3c2aaba6f757bf13f0c86a83335b34a1b2b5d72
sanjanalalwani <610sanjana@gmail.com>
2023-09-06 21:04:20 +0530

    init

--- law/src/screens/Profile.jsx
+++ law/src/screens/Profile.jsx
@@ -1,9 +1,12 @@
-import React , { Component } from 'react';
+import React , { useState } from 'react';
  import "../css/profile.css";
-
+ import Notes from '../screens/Notes.jsx';
+import Box from '../components/Component.jsx';
 // import { getAuth, onAuthStateChanged } from "firebase/auth";
 
-const Profile = () => {
+
+
+ const Profile = () => {
     // const auth = getAuth();
 
 // useEffect(() => {
@@ -19,16 +22,23 @@ const Profile = () => {
 
   return (
     <div class="wrapper">
-    <aside class="aside">Profile Page
+    <aside class="aside"><h3>Profile Page</h3>
     <br />
     <img src="https://img.freepik.com/premium-vector/account-icon-user-icon-vector-graphics_292645-552.jpg?" alt="" />
     </aside>
-    <div class="left">Your Notes
+    <div class="right"><h3>Scheduled Meetings</h3></div>
+    <div class="left">
+ 
+<Notes></Notes>
+
     </div>
-    <div class="content">Your Documents</div>
-    <div class="right">Scheduled Meetings</div>
+    <div class="content"><h3>Your Documents</h3></div>
+    
  </div>
   )
 }
 
 export default Profile
+
+
+
