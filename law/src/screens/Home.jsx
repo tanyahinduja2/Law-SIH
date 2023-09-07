2b4d9fa4f24bb16901dec4d91c8f26dca37fbf2d
Shreyans jain <shreyansjain1603@gmail.com>
2023-09-08 00:13:57 +0530

    commit

--- law/src/screens/Home.jsx
+++ law/src/screens/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Hero from '../components/Hero'
 import { Description } from '../components/Description'
+import Eden from '../components/Eden'
 export default function Home() {
   return (
 <div>
