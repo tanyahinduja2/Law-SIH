d4ca3a5ce7d60280d6a4b861148c3a1b99f648b1
tanyahinduja2 <tanyahinduja1@gmail.com>
2023-09-10 20:19:43 +0530

    commit

--- law/src/components/Footer.jsx
+++ law/src/components/Footer.jsx
@@ -1,27 +1,27 @@
 import React from "react";
 import "../css/footer.css";
-import "../index.css"
+import "../index.css";
+import { useNavigate } from "react-router-dom";
 
 const Footer = () => {
+    const navigate = useNavigate()
     return (
         <div className="footer">
             <h2 className="footer-logo">Law</h2>
             <div className="footer-ele">
-                <h4>Sitemap</h4>
-                <a href="#">Other Products</a>
-                <a href="#">News</a>
+                <h4>Templates</h4>
+                <a href="#" onClick={() => navigate('/templates')}>Generate Documents</a>
+                {/* <a href="#">News</a> */}
             </div>
             <div className="footer-ele">
-                <h4>Company</h4>
-                <a href="#">About Us</a>
-                <a href="#">Partners</a>
-                <a href="#">Contact Us</a>
-                <a href="#">Help</a>
+                <h4>Seek Assistance</h4>
+                <a href="#" onClick={() => navigate('/chatbot')}>Chat Now</a>
+                <a href="#"onClick={() => navigate('/maps')}>Explore</a>
+                <a href="#" onClick={() => navigate('/schedule')}>Schedule</a>
             </div>
             <div className="footer-ele">
                 <h4>Contact</h4>
-                <div>Level 8, 525 Flinders Street,
-                    Melbourne 3000 Vic</div>
+                <div>W, P. G. Kher Marg, 32nd Road, Marg, Off Linking Rd, TPS III, Bandra West, Mumbai, Maharashtra 400050</div>
                 <div>info@docscentre.com.au</div>
                 <div>1800 700 666</div>
             </div>
